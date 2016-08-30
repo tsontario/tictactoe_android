6d9724073c62993131f44a268989999793a3e5ae
Tim Smith <tsontario@gmail.com>
2016-08-30 12:25:44 -0400

    Add game functionality TODO Create gameover dialog

--- app/src/main/java/com/tictactoe/timothysmith/tictactoe/MainActivity.java
+++ app/src/main/java/com/tictactoe/timothysmith/tictactoe/MainActivity.java
@@ -1,5 +1,6 @@
 package com.tictactoe.timothysmith.tictactoe;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,23 +10,29 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     CurrentPlayer currentPlayer;
+    Button[][] gameBoard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         currentPlayer = CurrentPlayer.X;
+        gameBoard = new Button[3][3];
+        initializeGameBoard();
     }
 
     protected void turn(View view) {
 
         Button b = (Button) findViewById(view.getId());
+
         if (b.getText().equals("")) {
             b.setText(currentPlayer.toString());
-            switchPlayer();
+            if (hasWinCondition()) {
+                endGame();
+            } else {
+                switchPlayer();
+            }
         }
-
-
     }
 
     private void switchPlayer() {
@@ -38,6 +45,68 @@ public class MainActivity extends AppCompatActivity {
             currentPlayer = CurrentPlayer.X;
             toastString = "Player 'X's Turn";
         }
+
         Toast.makeText(MainActivity.this, toastString, Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean hasWinCondition() {
+        return (checkRows() || checkCols() || checkDiags());
+    }
+
+    private boolean checkRows() {
+        for (int i=0; i<3; i++) {
+            if (gameBoard[i][0].getText().equals("")) {
+                continue;
+            }
+            else if (gameBoard[i][0].getText().equals(gameBoard[i][1].getText()) &&
+                    gameBoard[i][0].getText().equals(gameBoard[i][2].getText())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkCols() {
+        for (int i=0; i<3; i++) {
+            if (gameBoard[0][i].getText().equals("")) {
+                continue;
+            }
+            else if (gameBoard[0][i].getText().equals(gameBoard[1][i].getText()) &&
+                    gameBoard[0][i].getText().equals(gameBoard[2][i].getText())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkDiags() {
+        if (gameBoard[1][1].getText().equals("")) {
+            return false;
+        }
+        if (gameBoard[0][0].getText().equals(gameBoard[1][1].getText()) &&
+                gameBoard[0][0].getText().equals(gameBoard[2][2].getText())) {
+
+            return true;
+        }
+        else if (gameBoard[0][2].getText().equals(gameBoard[1][1].getText()) &&
+                gameBoard[0][2].getText().equals(gameBoard[2][0].getText())) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void endGame() {
+        System.exit(1);
+    }
+
+    private void initializeGameBoard() {
+        for (int i=0; i<3; i++) {
+            for (int j=0; j<3; j++) {
+                String b = "button" + ((i*3)+j);  // Get Button ID #
+                gameBoard[i][j] = (Button) findViewById(getResources().
+                        getIdentifier(b, "id", getPackageName()));
+            }
+        }
     }
 }
