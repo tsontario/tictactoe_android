d7c9bfd5099fc7e6e276eb6c0021617de4818aa7
Tim Smith <tsontario@gmail.com>
2016-08-30 11:41:59 -0400

    Initial Button Functionality TODO check win condition

---
+++ app/src/main/java/com/tictactoe/timothysmith/tictactoe/CurrentPlayer.java
@@ -0,0 +1,8 @@
+package com.tictactoe.timothysmith.tictactoe;
+
+/**
+ * Created by timothysmith on 2016-08-30.
+ */
+public enum CurrentPlayer {
+    X, O;
+}
--- app/src/main/java/com/tictactoe/timothysmith/tictactoe/MainActivity.java
+++ app/src/main/java/com/tictactoe/timothysmith/tictactoe/MainActivity.java
@@ -2,12 +2,42 @@ package com.tictactoe.timothysmith.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    CurrentPlayer currentPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        currentPlayer = CurrentPlayer.X;
+    }
+
+    protected void turn(View view) {
+
+        Button b = (Button) findViewById(view.getId());
+        if (b.getText().equals("")) {
+            b.setText(currentPlayer.toString());
+            switchPlayer();
+        }
+
+
+    }
+
+    private void switchPlayer() {
+        String toastString = "";
+        if (currentPlayer == CurrentPlayer.X) {
+            currentPlayer = CurrentPlayer.O;
+            toastString = "Player 'O's Turn";
+        }
+        else {
+            currentPlayer = CurrentPlayer.X;
+            toastString = "Player 'X's Turn";
+        }
+        Toast.makeText(MainActivity.this, toastString, Toast.LENGTH_SHORT).show();
     }
 }
